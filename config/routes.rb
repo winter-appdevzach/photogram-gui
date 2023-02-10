31b673147be0f90c88d9f805cdebddefa94ee39e
Appdevzach <zmarciano@uchicago.edu>
2023-02-10 22:56:22 +0000

    progress. almost done with vid

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:path_username", {:controller => "users", :action => "show"})
+
   get("/photos", {:controller => "photos", :action => "index"})
-  get("/photos/:path_username", {:controller => "users", :action => "show"})
+  get("/photos/:path_id", {:controller => "photos", :action => "show"})
+
+  get("/delete_photo/:toast_id", {:controller => "photos", :action => "baii"})
+
 
 
 
